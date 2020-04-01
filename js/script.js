8d05ad8a135cfe00c57c72699b4811ab14690ae4
Mostafa Taha <mstf_deco@outlook.com>
2020-04-01 20:22:37 +0200

    Debloyment

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Swiper Slider
+
+var swiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 0,
+  mousewheel: true,
+  autoplay: {
+    delay: 10000
+  },
+  keyboard: true,
+  loop: true
+});
